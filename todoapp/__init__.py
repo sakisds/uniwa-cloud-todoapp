27994b97e996fccff4464837aecc9afcd25e9d7b
Thanasis Georgiou <ee06664@uniwa.gr>
2020-08-29 18:13:07 +0300

    Add a local store using sqlite

--- todoapp/__init__.py
+++ todoapp/__init__.py
@@ -1,6 +1,23 @@
+from datetime import datetime, date
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local_store.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To surpress warning
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
+    date_completed = db.Column(db.DateTime, default=date(2000, 1, 1))
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 
 @app.route("/")
--- todoapp/__main__.py
+++ todoapp/__main__.py
@@ -2,6 +2,21 @@
 Entrypoint for todo app
 '''
 
-from todoapp import serve
+from argparse import ArgumentParser
 
-serve()
+from todoapp import serve, db
+
+
+# Check CMD arguments
+argparser = ArgumentParser()
+argparser.add_argument("command", choices=['init-db', 'serve'])
+args = argparser.parse_args()
+
+command = args.command
+
+if command == 'serve':
+    print('Starting web server!')
+    serve()
+elif command == 'init-db':
+    print('Initializing database')
+    db.create_all()
